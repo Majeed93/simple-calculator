e973e77a9bb1f781292a7f75f64da7eb035656e7
Majeed Khan (MK) <mk.naghar@gmail.com>
2019-05-11 03:25:00 +0500

    Add files via upload

---
+++ table.py
@@ -0,0 +1,17 @@
+a = "majeed"
+print(type(a))
+first_num = int(input("Enter first num"))
+sec_num = int(input("Enter Second num"))
+operator = input("please enter following operator (+,-,*,/)")
+
+if operator =="/":
+    print(first_num*sec_num)
+
+if operator =="*":
+    print(first_num*sec_num)
+
+if operator =="-":
+    print(first_num-sec_num)
+
+if operator =="+":
+    print(first_num+sec_num)            
